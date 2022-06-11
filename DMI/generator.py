a94d282709a0727699102cf4f8c37c8e220c6d62
xpeng9719 <106721409+xpeng9719@users.noreply.github.com>
2022-06-11 21:24:27 +0800

    Update generator.py

--- DMI/generator.py
+++ DMI/generator.py
@@ -30,31 +30,6 @@ class Generator(nn.Module):
         y = self.l2_5(y)
         return y
 
-class GeneratorCIFAR(nn.Module):
-    def __init__(self, in_dim=100, dim=64):
-        super(GeneratorCIFAR, self).__init__()
-        def dconv_bn_relu(in_dim, out_dim):
-            return nn.Sequential(
-                nn.ConvTranspose2d(in_dim, out_dim, 5, 2,
-                                   padding=2, output_padding=1, bias=False),
-                nn.BatchNorm2d(out_dim),
-                nn.ReLU())
-
-        self.l1 = nn.Sequential(
-            nn.Linear(in_dim, dim * 4 * 4 * 4, bias=False),
-            nn.BatchNorm1d(dim * 4 * 4 * 4),
-            nn.ReLU())
-        self.l2_5 = nn.Sequential(
-            dconv_bn_relu(dim * 4, dim * 2),
-            dconv_bn_relu(dim * 2, dim),
-            nn.ConvTranspose2d(dim, 3, 5, 2, padding=2, output_padding=1),
-            nn.Sigmoid())
-
-    def forward(self, x):
-        y = self.l1(x)
-        y = y.view(y.size(0), -1, 4, 4)
-        y = self.l2_5(y)
-        return y
 
 class GeneratorMNIST(nn.Module):
     def __init__(self, in_dim=100, dim=64):
@@ -72,36 +47,6 @@ class GeneratorMNIST(nn.Module):
             nn.BatchNorm1d(dim * 4 * 4 * 4),
             nn.ReLU())
         self.l2_5 = nn.Sequential(
-            dconv_bn_relu(dim * 4, dim * 2),
-            dconv_bn_relu(dim * 2, dim),
-            nn.ConvTranspose2d(dim, 1, 5, 2, padding=2, output_padding=1),
-            nn.Sigmoid())
-
-    def forward(self, x):
-        y = self.l1(x)
-        y = y.view(y.size(0), -1, 4, 4)
-        y = self.l2_5(y)
-        return y
-
-
-class GeneratorChestX(nn.Module):
-    def __init__(self, in_dim=100, dim=64):
-        super(GeneratorChestX, self).__init__()
-
-        def dconv_bn_relu(in_dim, out_dim):
-            return nn.Sequential(
-                nn.ConvTranspose2d(in_dim, out_dim, 5, 2,
-                                   padding=2, output_padding=1, bias=False),
-                nn.BatchNorm2d(out_dim),
-                nn.ReLU())
-
-        self.l1 = nn.Sequential(
-            nn.Linear(in_dim, dim * 16 * 4 * 4, bias=False),
-            nn.BatchNorm1d(dim * 16 * 4 * 4),
-            nn.ReLU())
-        self.l2_5 = nn.Sequential(
-            dconv_bn_relu(dim * 16, dim * 8),
-            dconv_bn_relu(dim * 8, dim * 4),
             dconv_bn_relu(dim * 4, dim * 2),
             dconv_bn_relu(dim * 2, dim),
             nn.ConvTranspose2d(dim, 1, 5, 2, padding=2, output_padding=1),
